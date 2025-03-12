cbb6e793a23fc30b59e55efa46a542e79577cd53
Denys Dikhtiar <dinis.dihtjar08@gmail.com>
2025-03-12 17:57:50 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,13 @@
-# write your code here
+import pytest
+from app.main import check_password
+
+
+@pytest.mark.parametrize("password, valid",
+                         [
+                             ("Pass@word1", True),
+                             ("qwerty", False),
+                             ("qwertyuiopasdfghjkl", False),
+                             ("asdqwd", False)
+                         ])
+def test_our_funk(password: str, valid: bool) -> None:
+    assert check_password(password) == valid
